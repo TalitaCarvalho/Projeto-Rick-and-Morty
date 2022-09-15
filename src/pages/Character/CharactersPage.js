3b19ca039c2232b736bea706e25d881c5a7f6da3
TalitaCarvalho <talitadealmeidacarvalho9@outlook.com>
2022-09-15 16:18:57 -0300

    implementação das pages de personagens e dimensões e implementação do filtro de busca

--- src/pages/Character/CharactersPage.js
+++ src/pages/Character/CharactersPage.js
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 import CharactersList from "../../components/Characters/CharactersList";
 import Spinner from "../../components/App/Spinner";
 import service from "../../utils/service";
+import Icon from "@mdi/react";
+import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";
 
 const CharactersPage = () => {
   const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
 
   async function fetchCharacters() {
     const result = await service(`
     characters {
       info {
+        count,
         pages,
         next,
         prev
@@ -23,16 +31,106 @@ const CharactersPage = () => {
     }
     `);
     if (result) {
+      setInfo(result.data.characters.info);
       setCharacters(result.data.characters.results);
       setLoading(false);
     }
   }
 
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
+  async function fetchCharactersByPage(pageNumber) {
+    const result = await service(`
+    characters(page: ${pageNumber}){
+      info {
+        count,
+        pages,
+        next,
+        prev
+      }
+      results {
+        id,
+        name,
+        image
+      }
+    }
+    `);
+    if (result) {
+      setInfo(result.data.characters.info);
+      setCharacters(result.data.characters.results);
+      setLoading(false);
+    }
+  }
+
+  async function fetchSearchCharacter(searchCharacter) {
+    const result = await service(`
+    characters (filter: {name: "${searchCharacter}"}) {
+      info {
+        count,
+        pages,
+        next,
+        prev
+      }
+      results {
+        id,
+        name,
+        image
+      }
+    }
+    `);
+    if (result) {
+      setInfo(result.data.characters.info);
+      setCharacters(result.data.characters.results);
+    }
+  }
+
+  function handleSearchInput(searchValueId) {
+    fetchSearchCharacter(searchValueId);
+  }
+
+  function handlePageNavigation(action) {
+    if (action === "prev") {
+      fetchCharactersByPage(info.prev);
+      return;
+    }
+    fetchCharactersByPage(info.next);
+  }
+
+  const pageNumber = () => {
+    return info.next ? info.next - 1 : info.prev + 1;
+  };
+
   return (
-    <div>{loading ? <Spinner /> : <CharactersList results={characters} />}</div>
+    <>
+      <div className="mt-5 flex flex-row justify-center  ">
+        <input
+          type="text"
+          className="border-2 border-black-600 w-64 rounded-lg pl-2"
+          placeholder="Digite o nome de um personagem"
+          onChange={(ev) => handleSearchInput(ev.target.value)}
+        />
+      </div>
+
+      <div>
+        {loading ? <Spinner /> : <CharactersList results={characters} />}
+      </div>
+
+      <div className="flex flex-row justify-between items-center max-w-[50%] m-auto">
+        <button
+          className="flex flex-row items-center"
+          onClick={() => handlePageNavigation("prev")}
+        >
+          <Icon path={mdiChevronLeft} size={1.5} title="Anterior" />
+          Anterior
+        </button>
+        {pageNumber()}
+        <button
+          className="flex flex-row items-center"
+          onClick={() => handlePageNavigation("next")}
+        >
+          Próximo
+          <Icon path={mdiChevronRight} size={1.5} title="Próximo" />
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/pages/Dimension/DimensionsPage.js
+++ src/pages/Dimension/DimensionsPage.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 import DimensionsList from "../../components/Dimensions/DimensionsList";
 import { Fundo } from "../../components/Dimensions/DimensionsStyles";
-import service from "../../utils/service"
+import service from "../../utils/service";
+import Icon from "@mdi/react";
+import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";
 
 const DimensionsPage = () => {
-  const [locations, setLocations] = useState([])
+  const [locations, setLocations] = useState([]);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    fetchLocations();
+  }, []);
 
   async function fetchLocations() {
     const result = await service(`
     locations {
+      info {
+        count,
+        pages,
+        next,
+        prev
+      }
       results {
         id,
         name,
@@ -16,19 +29,105 @@ const DimensionsPage = () => {
       }
     }
     `);
-    setLocations(result.data.locations.results)
+    if (result) {
+      setInfo(result.data.locations.info);
+      setLocations(result.data.locations.results);
+    }
   }
-  
-  useEffect(() => {
-    fetchLocations()
-  }, [])
+
+  async function fetchLocationsByPage(pageNumber) {
+    const result = await service(`
+    locations(page: ${pageNumber}){
+      info {
+        count,
+        pages,
+        next,
+        prev
+      }
+      results {
+        id,
+        name
+      }
+    }
+    `);
+    if (result) {
+      setInfo(result.data.locations.info);
+      setLocations(result.data.locations.results);
+    }
+  }
+
+  async function fetchSearchLocation(searchLocation) {
+    const result = await service(`
+    locations (filter: {name: "${searchLocation}"}) {
+      info {
+        count,
+        pages,
+        next,
+        prev
+      }
+      results {
+        id,
+        name
+      }
+    }
+    `);
+    if (result) {
+      setInfo(result.data.locations.info);
+      setLocations(result.data.locations.results);
+    }
+  }
+
+  function handleSearchInput(searchValueId) {
+    fetchSearchLocation(searchValueId);
+  }
+
+  function handlePageNavigation(action) {
+    if (action === "prev") {
+      fetchLocationsByPage(info.prev);
+      return;
+    }
+    fetchLocationsByPage(info.next);
+  }
+
+  const pageNumber = () => {
+    return info.next ? info.next - 1 : info.prev + 1;
+  };
 
   return (
-    <section>
-      <Fundo>
-        <DimensionsList dimensions={locations} />
-      </Fundo>
-    </section>
+    <>
+      <div className="mt-5 flex flex-row justify-center  ">
+        <input
+          type="text"
+          className="border-2 border-black-600 w-64 rounded-lg pl-2"
+          placeholder="Digite o nome de uma dimensão"
+          onChange={(ev) => handleSearchInput(ev.target.value)}
+        />
+      </div>
+
+      <section>
+        <Fundo>
+          <DimensionsList dimensions={locations} />
+        </Fundo>
+
+        <div className="flex flex-row justify-between items-center max-w-[50%] m-auto">
+          <button
+            className="flex flex-row items-center"
+            onClick={() => handlePageNavigation("prev")}
+          >
+            <Icon path={mdiChevronLeft} size={1.5} title="Anterior" />
+            Anterior
+          </button>
+          {pageNumber()}
+          <button
+            className="flex flex-row items-center"
+            onClick={() => handlePageNavigation("next")}
+          >
+            Próximo
+            <Icon path={mdiChevronRight} size={1.5} title="Próximo" />
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
